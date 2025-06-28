eee9df1fbdb1debb0b006161e524cd98b8eb50c8
SuperMeerkat345 <artiom22903@gmail.com>
2025-06-28 00:06:10 -0400

    Added a way to automate the rating process of the images, added new prompts to suite 5 levels

--- scripts/generate_validation_csv.py
+++ scripts/generate_validation_csv.py
@@ -1,3 +1,26 @@
-import pandas from pd
+import pandas as pd
 import os
 
+# datapath should be to the ValidationSet
+def generate_validation_csv(dataPath, outputPath):
+    img_list = {
+        "label": [],
+        "path": []
+    }
+    
+    for tract in os.listdir(dataPath):
+        img_path = os.path.join(dataPath, tract)
+
+        for img in os.listdir(img_path):
+            # Full path to the image file
+            img_full_path = os.path.join(img_path, img)
+
+            img_list["path"].append(img_full_path)
+            img_list["label"].append("Null")
+    
+    csv = pd.DataFrame(img_list)
+    print(csv)
+    csv.to_csv(outputPath, index=False)
+            
+    
+generate_validation_csv("./ValidationSet", "./validation.csv")
